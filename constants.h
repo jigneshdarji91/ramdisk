54275cb6587431cb55c30cb2d6502b27ddc3a219
Jignesh Darji <jndarji@ncsu.edu>
2016-04-22 16:39:55 -0400

    adding constants to a single file

---
+++ constants.h
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  constants.h
+ *
+ *    Description:  constants that will be used for ramdisk
+ *
+ *        Version:  1.0
+ *        Created:  04/22/2016 
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jignesh Darji (jignesh), jndarji@ncsu.edu
+ *   Organization:  North Carolina State University
+ *
+ * =====================================================================================
+ */
+
+#ifndef __CONSTANTS__
+#define __CONSTANTS__
+
+#define MODE_FILE   0444
+#define MODE_DIR    0755
+#define MODE_ROOT   0777
+
+#endif
